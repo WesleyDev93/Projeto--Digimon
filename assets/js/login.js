057835a663dcb8303224fc819016158c5753f653
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-10-06 16:10:56 -0300

    implementando logica da pagina de login

---
+++ assets/js/login.js
@@ -0,0 +1,18 @@
+
+function validaDados () {
+
+    const login = document.querySelector ('.login').value;
+
+    const senha = document.querySelector ('.senha').value;
+
+
+
+    if (login == 'teste@gmail.com' && senha == '123456!') {
+        
+        alert ('Sua senha está correta -- Bem vindo Wesley');
+
+        location.href = "https://www.google.com/intl/pt-br/drive/about.html";
+    }else {
+        alert ('Sua senha esta errada  -- Administrador será avisado !');
+    }
+}
